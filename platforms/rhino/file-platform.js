28d13eb24f52c96edddf3ca3ab0f694651b805aa
kriskowal <kris.kowal@cixar.com>
2009-04-16 23:41:49 -0700

    Finished the standard library's file.open, with stubs for buffered/encoded IO.

--- platforms/rhino/file-platform.js
+++ platforms/rhino/file-platform.js
@@ -1,6 +1,9 @@
 // File: Rhino
 
 var IO = require("./io").IO;
+var file = require('file');
+
+/* deprecated File object */
 
 var File = exports.File = function(path, mode) {
     this.file = new java.io.File(path);
@@ -62,7 +65,28 @@ File.prototype.exists = function () {
     return this.file.exists();
 };
 
-var file = require('file');
+/* streams */
+
+exports.FileIO = function (path, mode, permissions) {
+    var {
+        read: read,
+        write: write,
+        append: append,
+        update: update
+    } = file.mode(mode);
+
+    if (update) {
+        throw new Error("Updating IO not yet implemented.");
+    } else if (write || append) {
+        return new IO(null, new Packages.java.io.FileOutputStream(path, append));
+    } else if (read) {
+        return new IO(new Packages.java.io.FileInputStream(path), null);
+    } else {
+        throw new Error("Files must be opened either for read, write, or update mode.");
+    }
+};
+
+/* paths */
 
 exports.SEPARATOR = '/';
 
@@ -77,39 +101,6 @@ var JavaPath = function (path) {
 exports.canonical = function (path) {
     return String(JavaPath(path).getCanonicalPath());
 };
-
-exports.exists = function (path) {
-    return JavaPath(path).exists();
-};
-
-exports.mkdir = function (path) {
-    if (!JavaPath(path).mkdir())
-        throw new Error("failed to make directory " + path);
-}
-
-exports.mkdirs = function(path) {
-    if (!JavaPath(path).mkdirs())
-        throw new Error("failed to make directories leading to " + path);
-}
-
-exports.rmdir = function(path) {
-    if (!JavaPath(Path)['delete']())
-        throw new Error("failed to remove the directory " + path);
-}
-
-exports.rmtree = function(path) {
-    if (!(path instanceof java.io.File)) {
-        path = JavaPath(path);
-    }
-    var files = path.listFiles();
-    files.forEach(function(f) {
-        if (f.isDirectory()) {
-            exports.rmtree(f);
-        } else {
-            f['delete']();
-        }
-    });
-}
 
 exports.mtime = function (path) {
     path = JavaPath(path);
@@ -129,6 +120,10 @@ exports.stat = function (path) {
         mtime: exports.mtime(path),
         size: exports.size(path)
     }
+};
+
+exports.exists = function (path) {
+    return JavaPath(path).exists();
 };
 
 exports.isDirectory = function (path) {
@@ -186,6 +181,35 @@ exports.remove = function (path) {
         throw new Error("failed to delete " + path);
 };
 
+exports.mkdir = function (path) {
+    if (!JavaPath(path).mkdir())
+        throw new Error("failed to make directory " + path);
+}
+
+exports.mkdirs = function(path) {
+    if (!JavaPath(path).mkdirs())
+        throw new Error("failed to make directories leading to " + path);
+}
+
+exports.rmdir = function(path) {
+    if (!JavaPath(Path)['delete']())
+        throw new Error("failed to remove the directory " + path);
+}
+
+exports.rmtree = function(path) {
+    if (!(path instanceof java.io.File)) {
+        path = JavaPath(path);
+    }
+    var files = path.listFiles();
+    files.forEach(function(f) {
+        if (f.isDirectory()) {
+            exports.rmtree(f);
+        } else {
+            f['delete']();
+        }
+    });
+}
+
 exports.list = function (path) {
     path = JavaPath(String(path));
     var listing = path.list();
@@ -199,32 +223,6 @@ exports.list = function (path) {
         paths[i] = String(listing[i]);
     }
 
-    var i = -1;
-
-    paths.next = function () {
-        i++;
-        if (i >= paths.length)
-            throw new Error("StopIteration");
-        return paths[i];
-    };
-
-    paths.prev = function () {
-        i--;
-        if (i < 0)
-            throw new Error("StopIteration");
-        return paths[i];
-    };
-
-    paths.iter = function () {
-        return paths;
-    };
-
-    paths.forEach = function (relation) {
-        for (var i = 0; i < paths.length; i++) {
-            relation(paths[i]);
-        }
-    };
-
     return paths;
 };
 
@@ -237,140 +235,4 @@ exports.touch = function (path, mtime) {
         throw new Error("unable to set mtime of " + path + " to " + mtime);
 };
 
-/*
-exports.open = function (path, mode, permissions, encoding, options) {
-    throw new Error("not yet implemented.");
-
-    path = JavaPath(path);
-    if (mode === undefined || permissions == null)
-        mode = "r";
-    mode = String(mode);
-    if (permissions == undefined || permissions == null)
-        permissions = 0777;
-    if (encoding === undefined || encoding === null)
-        encoding = 'binary';
-    options = options || {};
-
-    var read, write, append, noTruncate;
-    mode.split("").forEach(function (option) {
-        if (option == 'r') {
-            read = true;
-        } else if (option == 'w') {
-            write = true;
-        } else if (option == 'a') {
-            append = true;
-        } else if (option == '+') {
-            noTruncate = true;
-        } else {
-            throw new Error("unrecognized mode option in open: " + option);
-        }
-    });
-
-    var buffering = options.buffering;
-    var errors = options.errors;
-
-    if (read + write + append > 1) {
-        throw new Error("files can be opened for only one of read, write, or append modes.");
-    }
-    if (!(read || write || append)) {
-        throw new Error("file must be opened for read, write, or append mode.");
-    }
-    if (encoding == 'binary' && errors) {
-        throw new Error("binary encodingdoes not support error lists.");
-    }
-
-    var raw = FileIO(path, mode, permissions);
-    var lineBuffering = buffering == 1 || buffering === undefined && raw.isatty();
-
-    if (lineBuffering || buffering === undefined) {
-        buffering = 8 * 1024; // international standard buffer size
-        // try to set it to stat(path).blockSize
-    }
-    if (buffering < 0) {
-        throw new Error("invalid buffering size");
-    }
-    if (buffering === 0) {
-        if (encoding == "binary") {
-            return raw;
-        }
-        throw new Error("can't have unbuffered text IO");
-    }
-    if (encoding == "binary") {
-        throw new Error("encoding must not be binary to buffer.");
-    }
-
-    var buffer;
-    if (update) {
-        buffer = BufferedRandom(raw, buffering);
-    } else if (write || append) {
-        buffer = BufferedWriter(raw, buffering);
-    } else if (reading) {
-        buffer = BufferedReader(raw, buffering);
-    } else {
-        throw new Error("file must be opened for read, write, or append mode.");
-    }
-
-    return TextIOWrapper(buffer, encoding, errors, newLine, lineBuffering);
-
-};
-*/
-
-exports.open = function (path, mode, permissions, encoding, options) {
-
-    if (typeof path != 'string' && arguments.length == 1) {
-        options = path;
-        var {path: path} = options;
-    } else if (typeof path != 'string' && arguments.length == 2) {
-        options = mode;
-        var {path: path} = mode;
-    }
-
-    if (!mode) {
-        mode = 'r';
-    }
-
-    var reading, writing, appending, updating, truncating = true, canonical, exclusive;
-    mode.split("").forEach(function (option) {
-        if (option == 'r') {
-            reading = true;
-        } else if (option == 'w') {
-            writing = true;
-        } else if (option == 'a') {
-            appending = true;
-        } else if (option == '+') {
-            truncating = false;
-        } else if (option == 'c') {
-            canonical = true;
-        } else if (option == 'x') {
-            exclusive = true;
-        } else {
-            throw new Error("unrecognized mode option in open: " + option);
-        }
-    });
-
-    if (updating) {
-        throw new Error("NYI");
-    } else if (writing || appending) {
-        return {
-            'write': function (data) {
-                return File.write(path, data);
-            },
-            'flush': function () {
-            },
-            'close': function () {
-            }
-        }
-    } else if (reading) {
-        return {
-            'read': function () {
-                return File.read(path);
-            },
-            'close': function () {
-            }
-        }
-    } else {
-        throw new Error("NYI");
-    }
-
-};
 
