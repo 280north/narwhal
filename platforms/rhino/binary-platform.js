4a6e4cbdadda8a09b2b2ec4821afd0e2acac8f4f
kriskowal <kris.kowal@cixar.com>
2009-04-17 18:53:11 -0700

    Added a Base64 codec that can work with Binary.

--- platforms/rhino/binary-platform.js
+++ platforms/rhino/binary-platform.js
@@ -1,10 +1,30 @@
+
 var Binary = exports.Binary = function(bytes) {
+    if (bytes instanceof Array) {
+        var cast = Packages.java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, bytes.length);
+        for (var i = 0; i < bytes.length; i++) {
+            cast[i] = bytes[i] & 0xFF;
+        }
+        bytes = cast;
+    }
     this.bytes = bytes;
 }
 
 Binary.prototype.getLength = function() {
     return this.bytes.length;
 }
+
+Binary.prototype.charAt = function (i) {
+    return String.fromCharCode(this.bytes[i]);
+};
+
+Binary.prototype.byteAt = function (i) {
+    return String.fromCharCode(this.bytes[i]);
+};
+
+Binary.prototype.charCodeAt = function (i) {
+    return Number(this.bytes[i]);
+};
 
 Binary.prototype.toString = function(encoding) {
     return String(new java.lang.String(this.bytes, encoding || "UTF-8"));
