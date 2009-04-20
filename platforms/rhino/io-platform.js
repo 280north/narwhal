11b1ce8c013aa259be294db71203b0b9e5c8003e
Hannes Wallnöfer <hannesw@gmail.com>
2009-04-21 00:24:48 +0200

    Simplify implementation of IO.read() by using ByteArrayOutputStream

--- platforms/rhino/io-platform.js
+++ platforms/rhino/io-platform.js
@@ -9,72 +9,56 @@ var IO = exports.IO = function(inputStream, outputStream) {
 
 IO.prototype.read = function(length, encoding) {
     var readAll = false,
-        buffers = [],
         buffer  = null,
-        total   = 0,
+        bytes   = null,
         index   = 0,
-        read    = 0;
-    
+        total   = 0;
+
     if (typeof length !== "number") {
         readAll = true;
-        length = 1024;
+        bytes = new java.io.ByteArrayOutputStream();
+        buffer = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, 1024);
+    } else {
+        buffer = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, length);
     }
-        
+
     do {
-        if (!buffer)
-            buffer = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, length);
-        
-        read = this.inputStream.read(buffer, index, buffer.length - index);
-        
+        var read = this.inputStream.read(buffer, index, buffer.length - index);
+
         if (read < 0)
             break;
-        
+
+        if (readAll) {
+            bytes.write(buffer, index, read);
+            if (index >= buffer.length)
+                index = 0;
+        }
         index += read;
         total += read;
-        
-        if (index >= buffer.length) {
-            buffers.push(buffer);
-            buffer = null;
-            index = 0;
-            length *= 2;
-        }
-        
+
         //print("read="+read+" index="+index+" total="+total+" length="+length+" buffers.length="+buffers.length);
-        
-    } while (readAll && read > 0);
-    
+
+    } while ((readAll || total < length) && read > 0);
+
     var resultBuffer, resultLength;
-    
-    if (buffers.length === 1 && index === 0) {
-        resultBuffer = buffers[0]
-        resultLength = resultBuffer.length;
-    }
-    else {
-        resultBuffer = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, total),
-        resultLength = 0;
-            
-        for (var i = 0; i < buffers.length; i++) {
-            var buf = buffers[i];
-            java.lang.System.arraycopy(buf, 0, resultBuffer, resultLength, buf.length);
-            resultLength += buf.length;
-        }
-        
-        if (index > 0) {
-            java.lang.System.arraycopy(buffer, 0, resultBuffer, resultLength, index);
-            resultLength += index;
-        }
-    }
-    
+
+    if (readAll)
+        resultBuffer = bytes.toByteArray();
+    else
+        resultBuffer = buffer;
+
+    resultLength = resultBuffer.length;
+
     if (total != resultLength || total !== resultBuffer.length)
         throw new Error("IO.read sanity check failed: total="+total+" resultLength="+resultLength+" resultBuffer.length="+resultBuffer.length);
-        
+
     return new Binary(resultBuffer);
 }
 
 IO.prototype.write = function(object, encoding) {
     if (object === null || object === undefined || typeof object.toBinary !== "function")
         throw new Error("Argument to IO.write must have toBinary() method");
-    
+
     var binary = object.toBinary(encoding);
     this.outputStream.write(binary.bytes);
 }
