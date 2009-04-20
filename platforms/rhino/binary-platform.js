02927a1e0d886ae5e0fc94ef84de3cdb0a4b4c52
Hannes Wallnöfer <hannesw@gmail.com>
2009-04-21 00:52:27 +0200

    Accidentally removed String.prototype.toBinary()

--- platforms/rhino/binary-platform.js
+++ platforms/rhino/binary-platform.js
@@ -1,14 +1,21 @@
 var Binary = exports.Binary = function(bytes) {
     this.bytes = bytes;
-}
+};
 
 Binary.prototype.getLength = function() {
     return this.bytes.length;
-}
+};
 
 Binary.prototype.toString = function(encoding) {
     var jstr = encoding ?
                new java.lang.String(this.bytes, encoding) :
                new java.lang.String(this.bytes);
     return String(jstr);
-}
+};
+
+String.prototype.toBinary = function(encoding) {
+    var bytes = encoding ?
+                new java.lang.String(this).getBytes(encoding) :
+                new java.lang.String(this).getBytes();
+    return new Binary(bytes);
+};
