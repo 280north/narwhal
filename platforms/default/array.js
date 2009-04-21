61a7f2b87f452633cf752731843ec65338613e67
George Moschovitis <george-moschovitis@gmosx.com>
2009-04-21 20:40:47 +0300

    Added Array.map in default/array.js

--- platforms/default/array.js
+++ platforms/default/array.js
@@ -5,3 +5,20 @@ if (typeof Array.prototype.forEach !== "function")
 
 Array.isArray = function(obj) { return obj && typeof obj === "object" && obj.constructor === Array; }
 
+// https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference/Objects/Array/map
+if (!Array.prototype.map) {
+    Array.prototype.map = function(fun /*, thisp*/) {
+        var len = this.length >>> 0;
+        if (typeof fun != "function")
+          throw new TypeError();
+
+        var res = new Array(len);
+        var thisp = arguments[1];
+        for (var i = 0; i < len; i++) {
+            if (i in this)
+                res[i] = fun.call(thisp, this[i], i, this);
+        }
+
+        return res;
+    }
+}
