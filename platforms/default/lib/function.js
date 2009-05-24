e5fe6f3c66a22ccf48f59cf2487090753032c0bf
kriskowal <kris.kowal@cixar.com>
2009-05-24 03:18:24 -0700

    Fixed Function().bind idempotence.

--- platforms/default/lib/function.js
+++ platforms/default/lib/function.js
@@ -2,13 +2,15 @@
 Function.prototype.bind = function () {
     var args = Array.prototype.slice.call(arguments);
     var self = this;
+    if (self.bind)
+        return self;
     var block = function () {
         return self.call.apply(self, args);
     };
     block.name = this.name;
     block.displayName = this.displayName;
     block.length = this.length;
-    block.block = this;
+    block.bind = self;
     return block;
 };
 
