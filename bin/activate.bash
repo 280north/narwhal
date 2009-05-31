0a0d163d939f2875d1309df836561bb5a34083cb
kriskowal <kris.kowal@cixar.com>
2009-05-31 13:30:36 -0700

    Created bin/activate.bash.  Factored it out of bin/sea, and added activate.bash to tusk init.

---
+++ bin/activate.bash
@@ -0,0 +1,25 @@
+
+if [ -z "$PACKAGE_HOME" ]; then
+    PACKAGE_HOME=$(pwd)
+fi
+
+# which -s narwhal doesn't work (os x 10.5, kriskowal)
+if [ -f "$PACKAGE_HOME"/bin/narwhal ]; then
+    NARWHAL="$PACKAGE_HOME"/bin/narwhal
+else
+    env narwhal -e '' >/dev/null 2>&1
+    if [ "$?" -ne 127 ]; then
+        NARWHAL=narwhal
+    else
+        echo "ERROR: narwhal is not in your PATH or $PACKAGE_HOME/bin."
+        exit
+    fi
+fi
+
+if [ -f "$PACKAGE_HOME"/narwhal.conf ]; then
+    source "$PACKAGE_HOME"/narwhal.conf
+    export NARWHAL_DEFAULT_PLATFORM
+fi
+
+export PATH="$("$NARWHAL" --package "$PACKAGE_HOME" --path :)"
+
