7412b0bd8e0ae53b7971f814ed0e46639dd48a85
George Moschovitis <george-moschovitis@gmosx.com>
2009-06-01 21:06:00 +0300

    Added uri test case.

--- tests/serverjs-tests.js
+++ tests/serverjs-tests.js
@@ -2,5 +2,6 @@ exports.testByteString = require("./bytestring-tests");
 exports.testByteArray = require("./bytearray-tests");
 exports.testFile = require("./file-tests");
 exports.testString = require("./string-tests");
+exports.testURI = require("./uri-tests");
 
 require("os").exit(require("test/runner").run(exports));
