f7554dcd0b816a4a9f65ee7ba1c569557ac7394e
George Moschovitis <george-moschovitis@gmosx.com>
2009-06-01 20:56:15 +0300

    Added test case for string.

--- tests/serverjs-tests.js
+++ tests/serverjs-tests.js
@@ -1,5 +1,6 @@
 exports.testByteString = require("./bytestring-tests");
 exports.testByteArray = require("./bytearray-tests");
 exports.testFile = require("./file-tests");
+exports.testString = require("./string-tests");
 
 require("os").exit(require("test/runner").run(exports));
