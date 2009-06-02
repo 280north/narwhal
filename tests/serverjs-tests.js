9fdea4d2a714edecb0bd2d6a7c7a54b4b2423894
tlrobinson <tom@280north.com>
2009-06-01 17:38:25 -0700

    Reorganized ServerJS tests into it's own directory, and added beginning of module tests.

--- tests/serverjs-tests.js
+++
@@ -1,7 +0,0 @@
-exports.testByteString = require("./bytestring-tests");
-exports.testByteArray = require("./bytearray-tests");
-exports.testFile = require("./file-tests");
-exports.testString = require("./string-tests");
-exports.testURI = require("./uri-tests");
-
-require("os").exit(require("test/runner").run(exports));
