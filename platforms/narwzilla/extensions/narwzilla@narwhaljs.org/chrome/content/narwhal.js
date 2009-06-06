36804b62c1694de63e3eeaffb92c1d552a524075
Irakli Gozalishvili <rfobic@gmail.com>
2009-06-06 23:12:06 +0200

    splitting up bootloader from global exposer xpcom adding module and script in order to expose require, print, system to the scope

---
+++ platforms/narwzilla/extensions/narwzilla@narwhaljs.org/chrome/content/narwhal.js
@@ -0,0 +1,10 @@
+/**
+ * Script exposes narwhals system, require, print to the scope it is being included
+ * Simply include this file in your overlay.xml or main.xul file:
+ * @example
+ * <script src="chrome://narwzilla/content/narwhal.js" type="application/x-javascript"/>
+ * @author Irakli Gozalishvili <rfobic@gmail.com>
+ */
+var require = global.require;
+var print = global.print;
+var system = global.system;
