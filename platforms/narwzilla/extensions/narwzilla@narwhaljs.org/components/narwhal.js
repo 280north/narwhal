d4c8e1a67595988f3a04ecf0511fee9dc2ce5251
Irakli Gozalishvili <rfobic@gmail.com>
2009-06-06 19:57:40 +0200

    exposing Narwhal object to all the privileged scopes in xulrunner

--- platforms/narwzilla/extensions/narwzilla@narwhaljs.org/components/narwhal.js
+++ platforms/narwzilla/extensions/narwzilla@narwhaljs.org/components/narwhal.js
@@ -18,83 +18,59 @@ function getFileUri(file) {
         .getProtocolHandler('file').QueryInterface(Ci.nsIFileProtocolHandler)
         .getURLSpecFromFile(file);
 }
-/**
- *
- */
-var bootstrap = {};
-/**
- *
- */
-function Bootstrap() {}
-Bootstrap.prototype = {
-    classDescription: "Narwhal",
-    classID: Components.ID("{24e704fe-615d-7440-bcea-847795368b9e}"),
-    contractID: "@narwhaljs.org/narwzilla/bootstrap;1",
-    QueryInterface: XPCOMUtils.generateQI([
-        Ci.nsISupports,
-        Ci.nsIFactory,
-        Ci.nsICommandLineHandler,
-    ]),
-    _xpcom_categories: [{ category: "command-line-handler" }],
-    handle: function(cmdLine) {
-        var bootstrapFile;
-        // trying to get passed bootstrap.js (narwhal-narwzilla will pass it)
-        try { bootstrapFile = getFile(cmdLine.handleFlagWithParam('narwhal', false)); } catch (e) {}
-        // trying to read NARWHAL_HOME env variable
-        if (!bootstrapFile && cmdLine.handleFlag('narwhal', false)) {
-            try {
-            var path = Env.get('NARWHAL_HOME');
-            bootstrapFile = getFile(path, 'platforms', 'narwzilla', 'bootstrap.js');
-            } catch(e) {}
-        }
-        if (bootstrapFile && bootstrapFile.exists()) {
-            try {
-                Loader.loadSubScript(getFileUri(bootstrapFile), bootstrap);
-            } catch(e) {
-                dump('narwzilla> Error:' + e.message + '\nStack:' + e.stack + '\n');
-            }
-        }
-    },
-    helpInfo: '-narwhal              Starts narwhal\n',
-};
+// "{24e704fe-615d-7440-bcea-847795368b9e}"),
 
 var narwhal;
-function Narwhal() {}
+function Narwhal() {};
+Narwhal.Interfaces = [Ci.nsISupports, Ci.nsICommandLineHandler, Ci.nsIClassInfo, Ci.nsINarwhal];
 Narwhal.prototype = {
     classDescription: 'Narwhal',
     classID: Components.ID('{d438150e-51a2-4f45-9de9-619f5ab01a90}'),
     contractID: '@narwhaljs.org/narwzilla/narwhal;1',
-    QueryInterface: XPCOMUtils.generateQI([
-        Ci.nsISupports,
-        Ci.nsIClassInfo,
-        Ci.nsINarwhal
-    ]),
-    _xpcom_categories: [{
+    QueryInterface: XPCOMUtils.generateQI(Narwhal.Interfaces),
+    _xpcom_categories: [
+        { category: "command-line-handler" },
+        {
         // http://mxr.mozilla.org/seamonkey/source/dom/public/nsIScriptNameSpaceManager.h
         category: 'JavaScript global privileged property',
         entry: 'Narwhal'
-    }],
+        }
+    ],
     implementationLanguage: Ci.nsIProgrammingLanguage.JAVASCRIPT,
     getHelperForLanguage: function(number) null,
     _xpcom_factory: {
         createInstance: function(outer, iid) {
             if (outer != null) throw Components.results.NS_ERROR_NO_AGGREGATION;
             if (!narwhal) narwhal = new Narwhal();
-            narwhal._init(bootstrap);
-            narwhal.QueryInterface(Ci.nsINarwhal);
+            narwhal.QueryInterface(iid);
             return narwhal;
         }
     },
     getInterfaces: function(number) {
-        number.value = 1;
-        return [Ci.nsINarwhal];
+        number.value = Narwhal.Interfaces.length;
+        return Narwhal.Interfaces;
     },
-    _init: function(boot) {
-        this.require = boot.require;
-        this.print = boot.print;
-        this.system = boot.system;
-    }
+    handle: function(cmdLine) {
+        var bootstrap;
+        // trying to get passed bootstrap.js (narwhal-narwzilla will pass it)
+        try { bootstrap = getFile(cmdLine.handleFlagWithParam('narwhal', false)); } catch (e) {}
+        // trying to read NARWHAL_HOME env variable
+        if (!bootstrap && cmdLine.handleFlag('narwhal', false)) {
+            try {
+            var path = Env.get('NARWHAL_HOME');
+            bootstrap = getFile(path, 'platforms', 'narwzilla', 'bootstrap.js');
+            } catch(e) {}
+        }
+        if (bootstrap && bootstrap.exists()) {
+            try {
+                Loader.loadSubScript(getFileUri(bootstrap), this);
+            } catch(e) {
+                dump('narwzilla> Error:' + e.message + '\nStack:' + e.stack + '\n');
+            }
+        }
+    },
+    helpInfo: '-narwhal              Starts narwhal\n'
 };
 
-var components = [Bootstrap, Narwhal];
+var components = [Narwhal];
 function NSGetModule(compMgr, fileSpec) XPCOMUtils.generateModule(components);
