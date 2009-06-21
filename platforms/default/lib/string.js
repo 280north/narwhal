faf3242e9cc5285583c8f6a1087f14746cde8926
kriskowal <kris.kowal@cixar.com>
2009-06-21 03:01:57 -0700

    Consolidated date and string libs into the default platform globals, and renamed George's new String functions from startsWith/endsWith to the consistent metaphore begins/ends, and ltrim/rtrim to trimBegin and trimEnd, which are consistent with the names I provided in the util module.  Also applied some of Steven Levithan's magic to the trim functions, including cached regexen and chained replace calls.

--- platforms/default/lib/string.js
+++ platforms/default/lib/string.js
@@ -3,30 +3,30 @@
 String.prototype.forEach = function(block, separator) {
     system.print("WARNING: String.prototype.forEach deprecated");
     block(String(this)); // RHINO bug: it thinks "this" is a Java string (?!)
-}
+};
 
 String.prototype.squeeze = function() {
     var set = arguments.length > 0 ? "["+Array.prototype.join.apply(arguments, ["]|["])+"]" : ".|\\n",
         regex = new RegExp("("+set+")\\1+", "g");
     
     return this.replace(regex, "$1");
-}
+};
 
 String.prototype.chomp = function(separator) {
     var extra = separator ? separator + "|" : "";
     return this.replace(new RegExp("("+extra+"\\r|\\n|\\r\\n)*$"), "");
-}
+};
 
 // Check if the string starts with the given prefix string.
-String.prototype.startsWith = function(str) {
+String.prototype.begins = function(str) {
     return this.indexOf(str) === 0;
-}
+};
 
 // Check if the string ends with the given postfix string.
-String.prototype.endsWith = function(str) {
+String.prototype.ends = function(str) {
     var offset = this.length - str.length;
     return offset >= 0 && this.lastIndexOf(str) === offset;
-}
+};
 
 // Trim the string, left/right.
 //
@@ -39,17 +39,19 @@ String.prototype.endsWith = function(str) {
 // while (ws.test(str.charAt(--i)));
 // return str.slice(0, i + 1);
 // }
-String.prototype.trim = function() {
-	return this.replace(/^\s+|\s+$/g, "");
-}
-
 //Trim the string, left.
-String.prototype.ltrim = function() {
-	return this.replace(/^\s+/g, "");
-}
+var trimBeginExpression = /^\s\s*/g;
+String.prototype.trimBegin = function() {
+	return this.replace(trimBeginExpression, "");
+};
 
 // Trim the string, right.
-String.prototype.rtrim = function() {
-	return this.replace(/\s+$/g, "");
-}
+var trimEndExpression = /\s\s*$/g;
+String.prototype.trimEnd = function() {
+	return this.replace(trimEndExpression, "");
+};
+
+String.prototype.trim = function() {
+	return this.replace(trimBeginExpression, "").replace(trimEndExpression, "");
+};
 
