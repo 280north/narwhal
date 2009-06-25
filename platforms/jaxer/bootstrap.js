883bda523e50d742c540e283890a2bf3935b0b13
smith <nlloyds@gmail.com>
2009-06-25 14:01:57 -0500

    Copied template into jaxer

---
+++ platforms/jaxer/bootstrap.js
@@ -0,0 +1,35 @@
+(function (evalGlobal) {
+
+    var read = /*TODO*/; // function(path:string):string
+
+    var isFile = /*TODO*/; // function(path:string):boolean
+
+    var prefix = "/path/to/narwhal"; /*TODO*/
+
+    eval(read(prefix + "/narwhal.js"))({
+        global: this,
+        evalGlobal: evalGlobal,
+        platform: '<<<name>>>', /*TODO*/
+        platforms: ['<<<name>>>', 'default'], /*TODO*/
+        // XXX platforms may include any number of
+        // prioritized generic platforms like:
+        // rhino, java, c, v8, default
+        print: print,
+        evaluate: function (text) {
+            // TODO maybe something better here:
+            return eval(
+                "(function(require,exports,system,print){" +
+                text +
+                "/**/\n})"
+            );
+        },
+        fs: {
+            read: read,
+            isFile: isFile
+        },
+        prefix: prefix,
+    });
+
+}).call(this, function () {
+    return eval(arguments[0]);
+});
