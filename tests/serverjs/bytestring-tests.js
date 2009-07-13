b4f914f793cde3340216f653fc5729dd3cac0d79
Tom Robinson <tom@280north.com>
2009-07-13 14:34:39 -0700

    Split out binary tests that depend on character encoding features to make it easier to test new implementations.

--- tests/serverjs/bytestring-tests.js
+++ tests/serverjs/bytestring-tests.js
@@ -43,17 +43,7 @@ exports.testByteStringConstructor = function() {
     assert.isEqual(testArray.length, b3.length);
     assert.isEqual(1, b3.get(0));
     assert.isEqual(4, b3.get(3));
-    
-    // ByteString(string, charset)
-    // Convert a string. The ByteString will contain string encoded with charset.
-    var testString = "hello world";
-    var b4 = new ByteString(testString, "US-ASCII");
-    assert.isEqual(testString.length, b4.length);
-    b4.length = 123;
-    assert.isEqual(testString.length, b4.length);
-    assert.isEqual(testString.charCodeAt(0), b4.get(0));
-    assert.isEqual(testString.charCodeAt(testString.length-1), b4.get(testString.length-1));
-}
+};
 
 //exports.testByteStringJoin = function() {
 //}
@@ -66,10 +56,7 @@ exports.testToByteArray = function() {
     assert.isEqual(b1.length, b2.length);
     assert.isEqual(b1.get(0), b2.get(0));
     assert.isEqual(b1.get(2), b2.get(2));
-    
-    var testString = "I ♥ JS";
-    assert.isEqual(testString, new ByteString(testString, "UTF-8").toByteArray("UTF-8", "UTF-16").decodeToString("UTF-16"));
-}
+};
 
 exports.testToByteString = function() {
     var b1 = new ByteString([1,2,3]),
@@ -78,10 +65,7 @@ exports.testToByteString = function() {
     assert.isEqual(b1.length, b2.length);
     assert.isEqual(b1.get(0), b2.get(0));
     assert.isEqual(b1.get(2), b2.get(2));
-    
-    var testString = "I ♥ JS";
-    assert.isEqual(testString, new ByteString(testString, "UTF-8").toByteString("UTF-8", "UTF-16").decodeToString("UTF-16"));
-}
+};
 
 exports.testToArray = function() {
     var testArray = [0,1,254,255],
@@ -91,56 +75,22 @@ exports.testToArray = function() {
     assert.isEqual(testArray.length, a1.length);
     for (var i = 0; i < testArray.length; i++)
         assert.isEqual(testArray[i], a1[i]);
-    
-    a1 = new ByteString("\u0024\u00A2\u20AC", "UTF-8").toArray("UTF-8");
-    assert.isEqual(3, a1.length);
-    assert.isEqual(0x24, a1[0]);
-    assert.isEqual(0xA2, a1[1]);
-    assert.isEqual(0x20AC, a1[2]);
-    
-    a1 = new ByteString("\u0024\u00A2\u20AC", "UTF-16").toArray("UTF-16");
-    assert.isEqual(3, a1.length);
-    assert.isEqual(0x24, a1[0]);
-    assert.isEqual(0xA2, a1[1]);
-    assert.isEqual(0x20AC, a1[2]);
-}
+};
 
 exports.testToString = function() {
     // the format of the resulting string isn't specified, but it shouldn't be the decoded string
     // TODO: is this an ok test?
     
-    var testString = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"+
-        "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"+
-        "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"+
-        "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+    var testString = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"+
+                     "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+        testArray = [];
+    for (var i = 0; i < 128; i++) testArray.push(65);
     
-    var resultString = new ByteString(testString, "US-ASCII").toString();
+    var resultString = new ByteString(testArray).toString();
     
     assert.isTrue(resultString.length < 100);
     assert.isTrue(resultString !== testString);
-}
-
-exports.testDecodeToString = function() {
-    assert.isEqual("hello world", new ByteString("hello world", "US-ASCII").decodeToString("US-ASCII"));
-    
-    assert.isEqual("I ♥ JS", new ByteString("I ♥ JS", "UTF-8").decodeToString("UTF-8"));
-    
-    assert.isEqual("\u0024", new ByteString([0x24]).decodeToString("UTF-8"));
-    assert.isEqual("\u00A2", new ByteString([0xC2,0xA2]).decodeToString("UTF-8"));
-    assert.isEqual("\u20AC", new ByteString([0xE2,0x82,0xAC]).decodeToString("UTF-8"));
-    // FIXME:
-    //assert.isEqual("\u10ABCD", (new ByteString([0xF4,0x8A,0xAF,0x8D])).decodeToString("UTF-8"));
-    
-    assert.isEqual("\u0024", new ByteString("\u0024", "UTF-8").decodeToString("UTF-8"));
-    assert.isEqual("\u00A2", new ByteString("\u00A2", "UTF-8").decodeToString("UTF-8"));
-    assert.isEqual("\u20AC", new ByteString("\u20AC", "UTF-8").decodeToString("UTF-8"));
-    assert.isEqual("\u10ABCD", new ByteString("\u10ABCD", "UTF-8").decodeToString("UTF-8"));
-    
-    assert.isEqual("\u0024", new ByteString("\u0024", "UTF-16").decodeToString("UTF-16"));
-    assert.isEqual("\u00A2", new ByteString("\u00A2", "UTF-16").decodeToString("UTF-16"));
-    assert.isEqual("\u20AC", new ByteString("\u20AC", "UTF-16").decodeToString("UTF-16"));
-    assert.isEqual("\u10ABCD", new ByteString("\u10ABCD", "UTF-16").decodeToString("UTF-16"));
-}
+};
 
 exports.testIndexOf = function() {
     var b1 = new ByteString([0,1,2,3,4,5,0,1,2,3,4,5]);
@@ -158,7 +108,7 @@ exports.testIndexOf = function() {
     assert.isEqual(0,  b1.indexOf(0, 0, 3));
     assert.isEqual(-1,  b1.indexOf(5, 0, 3));
     assert.isEqual(-1, b1.indexOf(12, 0, 3));
-}
+};
 
 exports.testLastIndexOf = function() {
     var b1 = new ByteString([0,1,2,3,4,5,0,1,2,3,4,5]);
@@ -176,7 +126,7 @@ exports.testLastIndexOf = function() {
     assert.isEqual(6,  b1.lastIndexOf(0, 6, 9));
     assert.isEqual(-1,  b1.lastIndexOf(5, 6, 9));
     assert.isEqual(-1, b1.lastIndexOf(12, 6, 9));
-}
+};
 
 exports.testCharCodeAt = function() {
     var b1 = new ByteString([0,1,2,3,4,255]);
@@ -185,7 +135,7 @@ exports.testCharCodeAt = function() {
     assert.isEqual(0, b1.charCodeAt(0));
     assert.isEqual(255, b1.charCodeAt(5));
     assert.isTrue(isNaN(b1.charCodeAt(6)));
-}
+};
 
 // identical to charCodeAt, test anyway?
 exports.testGet = function() {
@@ -195,7 +145,7 @@ exports.testGet = function() {
     assert.isEqual(0, b1.get(0));
     assert.isEqual(255, b1.get(5));
     assert.isTrue(isNaN(b1.get(6)));
-}
+};
 
 exports.testByteAt = function() {
     var b1 = new ByteString([0,1,2,3,4,255]), b2;
@@ -210,7 +160,7 @@ exports.testByteAt = function() {
     assert.isEqual(255, b2.get(0));
     b2 = b1.byteAt(6);
     assert.isEqual(0, b2.length);
-}
+};
 
 // identical to byteAt, test anyway?
 exports.testCharAt = function() {
@@ -226,7 +176,7 @@ exports.testCharAt = function() {
     assert.isEqual(255, b2.get(0));
     b2 = b1.charAt(6);
     assert.isEqual(0, b2.length);
-}
+};
 
 exports.testSplit = function() {
     var b1 = new ByteString([0,1,2,3,4,5]), a1;
@@ -269,7 +219,7 @@ exports.testSplit = function() {
     assert.isEqual(2, a1[1].length);
     assert.isEqual(4, a1[1].get(0));
     assert.isEqual(5, a1[1].get(1));
-}
+};
 
 exports.testSlice = function() {
     var b1 = new ByteString([0,1,2,3,4,5]), b2;
@@ -301,11 +251,6 @@ exports.testSlice = function() {
     
     b2 = b1.slice(9, 10);
     assert.isEqual(0, b2.length);
-}
-
-exports.testStringToByteString = function() {
-    assert.isEqual("hello world", "hello world".toByteString("US-ASCII").decodeToString("US-ASCII"));
-    assert.isEqual("I ♥ JS", "I ♥ JS".toByteString("UTF-8").decodeToString("UTF-8"));
 };
 
 exports.testByteStringNewless = function () {
