c4e35f38b7e023788d0b38e82e53f27078c1ff5e
kriskowal <kris.kowal@cixar.com>
2009-08-03 15:19:06 -0700

    More JSON tool refinements.  More imperative in general.

--- tests/io/stringio.js
+++ tests/io/stringio.js
@@ -7,5 +7,15 @@ exports.testStringIODelimiter = function () {
     assert.eq(["a\0", "b\0"], stringio.readLines());
 };
 
+exports.testStringIODelimiterForEach = function () {
+    var stringio = new io.StringIO("a\0b\0", "\0");
+    var acc = [];
+    stringio.forEach(function (line) {
+        acc.push(line);
+    });
+    assert.eq(["a", "b"], acc);
+};
+
+
 if (require.main === module.id)
     require("os").exit(require("test/runner").run(exports));
