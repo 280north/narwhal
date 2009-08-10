b52a7a1ca2a13a55405dec3906486d9828c61911
George Moschovitis <george.moschovitis@gmail.com>
2009-08-10 10:53:49 +0300

    Added stubs for Object.defineProperty(), Object.defineProperties().

--- engines/default/lib/object.js
+++ engines/default/lib/object.js
@@ -25,3 +25,18 @@ if (!Object.freeze) {
     };
 }
 
+if (!Object.defineProperty) {
+    // WARNING: does not handle writable, enumerable, configurable
+    Object.defineProperty = function(obj, prop, desc) {
+        if (undefined != desc.value) obj[prop] = desc.value;
+        if ("function" == typeof(desc.get)) obj.__defineGetter__(prop, desc.get);
+        if ("function" == typeof(desc.set)) obj.__defineSetter__(prop, desc.set);
+    }
+}
+
+if (!Object.defineProperties) {
+    Object.defineProperties = function(obj, props) {    
+        for (var prop in props)
+            Object.defineProperty(obj, prop, props[prop]);
+    }    
+}
