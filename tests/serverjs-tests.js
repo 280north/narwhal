25e59f6345998392927526bee387c8f09d5f6318
kriskowal <kris.kowal@cixar.com>
2009-08-11 04:46:51 -0700

    Fixed the serverjs-tests module, forwarding to tests/serverjs/all-tests.

--- tests/serverjs-tests.js
+++ tests/serverjs-tests.js
@@ -1,6 +1,3 @@
-exports.testByteString = require("./bytestring-tests");
-exports.testByteArray = require("./bytearray-tests");
-exports.testFile = require("./file-tests");
-exports.testString = require("./string-tests");
-
-require("os").exit(require("test/runner").run(exports));
+exports.testServerJS = require("./serverjs/all-tests");
+if (require.main == module.id)
+    require("os").exit(require("test/runner").run(exports));
