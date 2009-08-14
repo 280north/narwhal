6d5af5f6f64e9ae4d23091405459e05c586995f8
kriskowal <kris.kowal@cixar.com>
2009-08-15 07:47:42 +0800

    Explicated the intent of isArray, isArrayLike, and isArguments through unit tests.
    Signed-off-by: Christoph Dorn <christoph@christophdorn.com>

---
+++ tests/global/array.js
@@ -0,0 +1,19 @@
+
+var assert = require('test/assert');
+
+exports.testIsArray = function () {
+    assert.isTrue(Array.isArray([]));
+};
+
+exports.testIsArrayNegativeArguments = function () {
+    var args = (function () {return arguments})();
+    assert.isFalse(Array.isArray(args));
+};
+
+exports.testIsArrayNegativeObject = function () {
+    assert.isFalse(Array.isArray({"length": 0}));
+};
+
+if (require.main === module.id)
+    require("os").exit(require("test/runner").run(exports));
+
