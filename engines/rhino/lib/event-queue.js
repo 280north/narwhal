87c19e9b835c1ce75fb9d0837359d2bb32906a4f
Kris Zyp <kriszyp@gmail.com>
2009-10-02 07:41:15 -0600

    Tabs to spaces

--- engines/rhino/lib/event-queue.js
+++ engines/rhino/lib/event-queue.js
@@ -6,13 +6,13 @@
 var queue = new java.util.concurrent.LinkedBlockingQueue();
 
 exports.nextEvent = function(){
-	return queue.take();
+    return queue.take();
 };
 
 exports.enqueue = function(task, priority){
-	queue.put(task); // priority is ignored for now until PriorityBlockingQueue is used
+    queue.put(task); // priority is ignored for now until PriorityBlockingQueue is used
 };
 
 exports.isEmpty = function(){
-	return queue.isEmpty();	
+    return queue.isEmpty();    
 }
