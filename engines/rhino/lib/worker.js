74b2ab716619d46f878653b5fb9999272d8570d3
Kris Zyp <kriszyp@gmail.com>
2009-10-07 12:01:24 -0600

    SharedWorker is implemented Fixed tabs -> spaces

--- engines/rhino/lib/worker.js
+++ engines/rhino/lib/worker.js
@@ -3,17 +3,17 @@ var queue = require("event-queue");
 var Worker = exports.Worker = function(scriptName){
     var worker;
     createWorker(scriptName, function(workerQueue, global){
-    	worker = createPort(workerQueue, global);
-    	createPort(queue, worker, global);
-    	return worker;
+        worker = createPort(workerQueue, global);
+        createPort(queue, worker, global);
+        return worker;
     });
     return worker;
 };
 
 function createPort(queue, target, port){
-	target.onmessage = true; // give it something to feature detect off of
-	port = port || {
-		// allows for sending a message with a direct object reference.
+    target.onmessage = true; // give it something to feature detect off of
+    port = port || {
+            // allows for sending a message with a direct object reference.
             // this is not part of CommonJS, and must be used with great care.
             __enqueue__: function(eventName, args){
                 queue.enqueue(function(){
@@ -23,43 +23,43 @@ function createPort(queue, target, port){
                 });
             }
         };
-        port.postMessage = function(message){
-                queue.enqueue(function(){
-                    var event = {
-                    	//when is supposed to be the target, and when the ports? The spec is confusing
-                    	target: target,
-                        ports: [target],
-                        data: message.toString(),
-                    }
-                    if(typeof target.onmessage == "function"){
-                        target.onmessage(event);
-                    }
-                });
-            };
-        port.postData= function(message){
-                queue.enqueue(function(){
-                    var event = {
-                        ports: [target],
-                        // this can be optimized to be much faster
-                        data: target.JSON.parse(JSON.stringify(message)), 
-                    }
-                    if(typeof target.ondata == "function"){
-                        target.ondata(event);
-                    }
-                });
-            };
-        port.isIdle= function(){
-                return queue.isEmpty();
-            };
-        return port;
+    port.postMessage = function(message){
+        queue.enqueue(function(){
+            var event = {
+                //when is supposed to be the target, and when the ports? The spec is confusing
+                target: target,
+                ports: [target],
+                data: message.toString(),
+            }
+            if(typeof target.onmessage == "function"){
+                target.onmessage(event);
+            }
+        });
+        };
+    port.postData= function(message){
+        queue.enqueue(function(){
+            var event = {
+                ports: [target],
+                // this can be optimized to be much faster
+                data: target.JSON.parse(JSON.stringify(message)), 
+            }
+            if(typeof target.ondata == "function"){
+                target.ondata(event);
+            }
+        });
+        };
+    port.isIdle= function(){
+        return queue.isEmpty();
+    };
+    return port;
 }
 function createWorker(scriptName, setup){
-        var workerQueue, 
-        	workerGlobal = new org.mozilla.javascript.tools.shell.Global();
-        javaWorkerGlobal = new org.mozilla.javascript.NativeJavaObject(global, workerGlobal, null);
-        javaWorkerGlobal.init(org.mozilla.javascript.tools.shell.Main.shellContextFactory);
-        workerGlobal.NARWHAL_HOME = system.prefix;
-        workerGlobal.NARWHAL_ENGINE_HOME = system.enginePrefix;
+    var workerQueue, 
+        workerGlobal = new org.mozilla.javascript.tools.shell.Global();
+    javaWorkerGlobal = new org.mozilla.javascript.NativeJavaObject(global, workerGlobal, null);
+    javaWorkerGlobal.init(org.mozilla.javascript.tools.shell.Main.shellContextFactory);
+    workerGlobal.NARWHAL_HOME = system.prefix;
+    workerGlobal.NARWHAL_ENGINE_HOME = system.enginePrefix;
     // get the path to the bootstrap.js file
     var bootstrapPath = system.enginePrefix + "/bootstrap.js";
     org.mozilla.javascript.tools.shell.Main.processFile(
@@ -72,67 +72,70 @@ function createWorker(scriptName, setup){
     workerQueue = workerGlobal.require("event-queue");
     var worker = setup(workerQueue, workerGlobal);
     workerGlobal.require("system").__sharedWorkers__ = system.__sharedWorkers__;
-	var thread = new java.lang.Thread(function(){
-		workerGlobal.require(scriptName);
-		while(true){
-		    try{
-			workerQueue.nextEvent()();
-			if(workerQueue.isEmpty()){
-			    queue.enqueue(function(){
-				if(worker && worker.onidle){
-					worker.onidle();
-				}
-			    });
-			}
-		    }catch(e){
-		    	workerQueue.enqueue(function(){
-		    		if(typeof workerGlobal.onerror === "function"){
-		    			try{
-		    				workerGlobal.onerror(e);
-		    			}
-		    			catch(e){
-		    				// don't want an error here to go into an infinite loop!
-		    				print(e);
-		    			}
-		    		}
-		    		else{
-		    			print(e);
-		    		}
-		    	});
-			
-			
-		    }
-		}
-	}, "Worker thread");
-	thread.start();
+    var thread = new java.lang.Thread(function(){
+        workerGlobal.require(scriptName);
+        while(true){
+            try{
+                workerQueue.nextEvent()();
+                if(workerQueue.isEmpty()){
+                    queue.enqueue(function(){
+                       if(worker && worker.onidle){
+                           worker.onidle();
+                       }
+                    });
+                }
+            }catch(e){
+                workerQueue.enqueue(function(){
+                    if(typeof workerGlobal.onerror === "function"){
+                        try{
+                            workerGlobal.onerror(e);
+                        }
+                        catch(e){
+                            // don't want an error here to go into an infinite loop!
+                            print((e.rhinoException && e.rhinoException.printStackTrace()) || (e.name + ": " + e.message));
+                        }
+                    }
+                    else{
+                        print((e.rhinoException && e.rhinoException.printStackTrace()) || (e.name + ": " + e.message));
+                    }
+                });
+            
+            
+            }
+        }
+    }, "Worker thread");
+    thread.start();
 };
 
 if(!system.__sharedWorkers__){
-	system.__sharedWorkers__ = {};
+    system.__sharedWorkers__ = {};
 }
 
 exports.SharedWorker = function(scriptName, workerName){
-	workerName = workerName || scriptName;
-	var shared = system.__sharedWorkers__[workerName];
-	if(!shared){
-		var shared = {};
-		createWorker(scriptName, function(queue, global){
-			shared.queue = queue;
-			shared.global = global;
-		});
-		system.__sharedWorkers__[workerName] = shared;
-	}
-	var port = {};
-	var returnPort = createPort(queue, port);
-	createPort(shared.queue, returnPort, port);
-	shared.global.onconnect = true;
-	shared.queue.enqueue(function(){
-		if(typeof shared.global.onconnect == "function"){
-			shared.global.onconnect({
-				port: port
-			});
-		}
-	});
-	return {port: port};
+    workerName = workerName || scriptName;
+    var shared = system.__sharedWorkers__[workerName];
+    if(!shared){
+        var shared = {};
+        createWorker(scriptName, function(queue, global){
+            shared.queue = queue;
+            shared.global = global;
+            global.onconnect = true;
+        });
+        system.__sharedWorkers__[workerName] = shared;
+    }
+    var port = {};
+    
+    var returnPort = createPort(queue, port);
+    
+    createPort(shared.queue, returnPort, port);
+    shared.queue.enqueue(function(){
+        if(typeof shared.global.onconnect == "function"){
+            shared.global.onconnect({
+                port: returnPort
+            });
+        }
+    });
+    shared.num = shared.num || Math.random();
+    return {port: port};
 }
 
