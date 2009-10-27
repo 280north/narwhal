5cc3fc003a8b8e1bc7d8d6b4aa760ff7f04ce9c6
nevilleburnell <neville.burnell@gmail.com>
2009-10-27 13:08:08 +1100

    add tests/file/dirname.js to expose issue 46

---
+++ tests/file/dirname.js
@@ -0,0 +1,19 @@
+
+var assert = require("test/assert");
+var file = require("file");
+var util = require("util");
+
+util.forEachApply([
+    ["/", "/"],
+    ["/a", "/a"],
+    ["/a/b", "/a"]
+
+], function (path, expected) {
+    exports['test ' + util.repr(arguments)] = function () {
+        assert.eq(expected, file.dirname(path));
+    };
+});
+
+if (require.main === module.id)
+    require("os").exit(require("test/runner").run(exports));
+
