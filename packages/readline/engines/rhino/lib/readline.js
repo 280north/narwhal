ca7960e67e2410378575cf5497ca678116c4e8d7
Kris Zyp <kriszyp@gmail.com>
2009-11-12 10:03:17 -0700

    Fixes tabs/spaces

--- packages/readline/engines/rhino/lib/readline.js
+++ packages/readline/engines/rhino/lib/readline.js
@@ -1,15 +1,19 @@
-// use the 
-var reader = new java.io.BufferedReader(new java.io.InputStreamReader(new org.mozilla.javascript.NativeJavaObject(global, org.mozilla.javascript.tools.shell.Main.global, null).getIn(), "UTF-8"));
+// use the Rhino shell, in case stdin is coming from the debugger GUI (this will still use jline)
+var reader = new java.io.BufferedReader(
+        new java.io.InputStreamReader(
+            new org.mozilla.javascript.NativeJavaObject(
+                global, org.mozilla.javascript.tools.shell.Main.global, null).
+                    getIn(), "UTF-8"));
 
 exports.readline = function() {
-	var line = reader.readLine();
-	if(line === null){
-		// jline will fail in eclipse, revert to the default impl
-		exports.readline = function(){
-			return system.stdin.readLine();
-		};
-		return exports.readline();
-	}
-	
+    var line = reader.readLine();
+    if(line === null){
+        // jline will fail in eclipse, revert to the default impl
+        exports.readline = function(){
+            return system.stdin.readLine();
+        };
+        return exports.readline();
+    }
+    
     return String(line);
 }
