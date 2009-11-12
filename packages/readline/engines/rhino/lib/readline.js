08d38fe7be7b4a9b0f5c5ece89d7e6a448b5aa16
Kris Zyp <kriszyp@gmail.com>
2009-11-11 21:25:49 -0700

    Deal with jline failure in eclipse

--- packages/readline/engines/rhino/lib/readline.js
+++ packages/readline/engines/rhino/lib/readline.js
@@ -2,5 +2,14 @@
 var reader = new java.io.BufferedReader(new java.io.InputStreamReader(new org.mozilla.javascript.NativeJavaObject(global, org.mozilla.javascript.tools.shell.Main.global, null).getIn(), "UTF-8"));
 
 exports.readline = function() {
-    return String(reader.readLine());
+	var line = reader.readLine();
+	if(line === null){
+		// jline will fail in eclipse, revert to the default impl
+		exports.readline = function(){
+			return system.stdin.readLine();
+		};
+		return exports.readline();
+	}
+	
+    return String(line);
 }
