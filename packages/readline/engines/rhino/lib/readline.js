7bbdce2ed704503f0156ef6a59da08a801a722df
Kris Zyp <kriszyp@gmail.com>
2009-11-11 21:16:14 -0700

    Use the Rhino's shell input, rather than going directly to jline, so that jline works in both debugger and command line

--- packages/readline/engines/rhino/lib/readline.js
+++ packages/readline/engines/rhino/lib/readline.js
@@ -1,5 +1,5 @@
-
-var reader = new Packages.jline.ConsoleReader();
+// use the 
+var reader = new java.io.BufferedReader(new java.io.InputStreamReader(new org.mozilla.javascript.NativeJavaObject(global, org.mozilla.javascript.tools.shell.Main.global, null).getIn(), "UTF-8"));
 
 exports.readline = function() {
     return String(reader.readLine());
