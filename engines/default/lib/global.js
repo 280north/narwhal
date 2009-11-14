3db2d8abd2d57cef80242d746fb5d4f7c797272f
kriskowal <kris.kowal@cixar.com>
2009-11-13 23:56:01 -0800

    Factored non-compliant CommonJS monkey patching into a narwhal-moar-monkeys package.

--- engines/default/lib/global.js
+++ engines/default/lib/global.js
@@ -1,5 +1,17 @@
 
 require("./global-es5");
 require("./global-commonjs");
-require("./global-moar-monkeys");
+
+// RegExp
+// ======
+
+/*** RegExp.escape
+    accepts a string; returns the string with regex metacharacters escaped.
+    the returned string can safely be used within a regex to match a literal
+    string. escaped characters are [, ], {, }, (, ), -, *, +, ?, ., \, ^, $,
+    |, #, [comma], and whitespace.
+*/
+RegExp.escape = function (str) {
+    return str.replace(/[-[\]{}()*+?.\\^$|,#\s]/g, "\\$&");
+};
 
