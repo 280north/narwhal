4fd5dc702c8bde1c25028bf7d2ff6769f2e6b172
Tom Robinson <tom@280north.com>
2009-11-19 17:25:36 -0800

    Revert "Avoids overwriting native JSON methods"
    This reverts commit 642e09f8a8213d1b18dd71170853a9a1808f8415.

--- engines/default/lib/json.js
+++ engines/default/lib/json.js
@@ -157,13 +157,8 @@
 
 // Create a JSON object only if one does not already exist. We create the
 // methods in a closure to avoid creating global variables.
-if(this.JSON){
-    exports.parse = JSON.parse;
-    exports.stringify = JSON.stringify;
-}
-else{
-    this.JSON = typeof exports == "undefined" ? {} : exports;
-}
+
+var JSON = this.JSON = typeof exports == "undefined" ? {} : exports;
 
 (function () {
 
