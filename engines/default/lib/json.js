fc6c1965e5b8be22127b6cb4090647a96245c0eb
kriskowal <kris.kowal@cixar.com>
2009-12-12 14:35:21 -0800

    Vain attempts to fix JSON parsing narwhal/package.json

--- engines/default/lib/json.js
+++ engines/default/lib/json.js
@@ -157,13 +157,12 @@
 
 // Create a JSON object only if one does not already exist. We create the
 // methods in a closure to avoid creating global variables.
-if(global.JSON){
+if (global.JSON) {
     exports.parse = JSON.parse;
     exports.stringify = JSON.stringify;
     exports.decode = JSON.parse;
     exports.encode = JSON.stringify;
-}
-else{
+} else {
     global.JSON = typeof exports == "undefined" ? {} : exports;
 }
 
