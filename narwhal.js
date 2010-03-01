fb5eb80a3325778a225cd023222290bfbc2af259
kriskowal <kris.kowal@cixar.com>
2010-02-28 21:21:49 -0800

    Began refactoring Narwhal's event loop story.

--- narwhal.js
+++ narwhal.js
@@ -256,7 +256,7 @@ if (options.interactive) {
 
 // send an unload event if that module has been required
 if (require.loader.isLoaded("unload")) {
-    require("unload").send();
+    require("unload").emit();
 }
 
 })
