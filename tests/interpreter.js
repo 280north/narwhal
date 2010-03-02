35d136c29bb529d6b011b39f2bc8d90d2d7d91a7
Tom Robinson <tom@280north.com>
2010-03-01 18:06:05 -0800

    Interpreter module primordial tests

--- tests/interpreter.js
+++ tests/interpreter.js
@@ -4,14 +4,14 @@ var Context = require("interpreter").Context;
 
 exports.testContextGlobalAssignment = function() {
     var c = new Context();
-    
+
     ASSERT.isTrue(typeof global.foo === "undefined");
     ASSERT.isTrue(!c.global.foo);
-    
+
     var testObj = {};
-    
+
     c.global.foo = testObj;
-    
+
     ASSERT.isTrue(typeof global.foo === "undefined");
     ASSERT.eq(testObj, c.global.foo);
     ASSERT.eq(testObj, c.eval("foo"));
@@ -20,12 +20,12 @@ exports.testContextGlobalAssignment = function() {
 
 exports.testContextEval = function() {
     var c = new Context();
-    
+
     ASSERT.isTrue(typeof global.foo === "undefined");
     ASSERT.isTrue(!c.global.foo);
-    
+
     c.eval("foo = 1234;");
-    
+
     ASSERT.isTrue(typeof global.foo === "undefined");
     ASSERT.eq(1234, c.global.foo);
     ASSERT.eq(1234, c.eval("foo"));
@@ -34,14 +34,14 @@ exports.testContextEval = function() {
 
 exports.testContextFunction = function() {
     var c = new Context();
-    
+
     ASSERT.isTrue(typeof global.foo === "undefined");
     ASSERT.isTrue(!c.global.foo);
-    
+
     var testObj = {};
-    
+
     (new c.Function("bar", "foo = bar;"))(testObj);
-    
+
     ASSERT.isTrue(typeof global.foo === "undefined");
     ASSERT.eq(testObj, c.global.foo);
     ASSERT.eq(testObj, c.eval("foo"));
@@ -50,22 +50,31 @@ exports.testContextFunction = function() {
 
 exports.testContextGlobal = function() {
     var c = new Context();
-    
+
     ASSERT.eq(c.eval("(function(){ return this; })()"), c.global);
-    
-    ASSERT.eq(c.eval("(this)"), c.global);
-    
+
+    ASSERT.eq(c.eval("this"), c.global);
+
     ASSERT.isTrue(c.eval("(function(){ return this; })() === this"));
+
+    ASSERT.isTrue(global !== c.global, "Context global should be unique.");
 }
 
-exports.testContextDifferentPrimordials = function() {
+exports.testContextPrimordials = function() {
     var c = new Context();
-    
-    ASSERT.eq(c.eval("(function(){ return this; })()"), c.global);
-    
-    ASSERT.eq(c.eval("(this)"), c.global);
-    
-    ASSERT.isTrue(c.eval("(function(){ return this; })() === this"));
+
+    var globalNames = [
+        "Array", "Boolean", "Date", "Error", "EvalError", "Function",
+        "Math", "Number", "Object", "RangeError", "ReferenceError",
+        "RegExp", "String", "SyntaxError", "TypeError", "URIError",
+        "decodeURI", "decodeURIComponent", "encodeURI", "encodeURIComponent",
+        "eval", "isFinite", "isNaN", "parseFloat", "parseInt",
+    ];
+
+    globalNames.forEach(function(globalName) {
+        ASSERT.isTrue(!!c.global[globalName], globalName + " global should exist");
+        ASSERT.isTrue(c.global[globalName] !== global[globalName], globalName + " global should be different than parent Context's");
+    });
 }
 
 if (require.main == module)
