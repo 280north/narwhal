b774cc31e685b9116dd0f1facce03aa19340e671
Tom Robinson <tom@280north.com>
2010-04-28 16:13:28 -0700

    More tlrobinson->280north references

--- js/misc.js
+++ js/misc.js
@@ -1,10 +1,10 @@
 function showreleases(response) {
     var releases = [];
     for (var tag in response.tags) {
-        var tar = "http://github.com/tlrobinson/narwhal/tarball/"+tag,
-            zip = "http://github.com/tlrobinson/narwhal/zipball/"+tag;
-            commit = "http://github.com/tlrobinson/narwhal/commit/"+response.tags[tag],
-            tree = "http://github.com/tlrobinson/narwhal/tree/"+response.tags[tag];
+        var tar = "http://github.com/280north/narwhal/tarball/"+tag,
+            zip = "http://github.com/280north/narwhal/zipball/"+tag;
+            commit = "http://github.com/280north/narwhal/commit/"+response.tags[tag],
+            tree = "http://github.com/280north/narwhal/tree/"+response.tags[tag];
         releases.push('<li>'+tag+': [<a href="'+zip+'">zip</a>][<a href="'+tar+'">tar</a>][<a href="'+tree+'">tree</a>][<a href="'+commit+'">commit</a>]</li>');
     }
     releases = releases.sort();
